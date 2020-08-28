ff059e54d537a054c136931f96213b9a4ca27cc3
Vladimir Skoblo <v.skoblo@gmail.com>
2020-08-28 10:32:14 +0300

    rework instantiate ball

--- Assets/Scripts/LaunchBallController.cs
+++ Assets/Scripts/LaunchBallController.cs
@@ -1,16 +1,18 @@
 using System;
 using JetBrains.Annotations;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 internal class LaunchBallController : MonoBehaviour
 {
     [SerializeField] private Transform launchPoint;
+    [SerializeField] private Ball[] balls;
 
     internal event Action<Ball> OnBallLaunched;
 
     internal void LaunchBall(float force)
     {
-        var ball = Instantiate(Resources.Load<Ball>("Ball"));
+        var ball = Instantiate(balls[Random.Range(0, balls.Length - 1)]);
 
         ball.transform.position = launchPoint.position;
 
